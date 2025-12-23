327f0ad18fc072e299f6791bbafb766aec15f4ec
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-23 12:05:57 +0530

    used map and props to render the component

--- props-practise/src/App.jsx
+++ props-practise/src/App.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
+
+  const heroes=["ShadowStrike","IronFalcon","NightBlaze","ThunderClaw","NovaKnight","PhantomX","SolarFury","DarkSpecter","StormRider","CrimsonVigil"];
+
+
   return (
-    <div className='w-screen h-screen bg-[#262626] flex items-center justify-center gap-5'>
-      <Card user="Kim ji-won"/>
+    <div className='w-screen h-screen bg-[#262626] flex flex-wrap p-4 gap-5 text-white'>
+      {heroes.map((elem)=>{
+        return <Card user={elem} />
+      })}
     </div>
   )
 }
--- props-practise/src/components/Card.jsx
+++ props-practise/src/components/Card.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Card = (props) => {
     console.log(props)
   return (
-    <div className=' p-5 flex items-center justify-center bg-transparent border-2 border-white rounded-sm text-white'>
-        <h1 className='text-6xl text-center '>{props.user}</h1>
+    <div className=' p-5 w-[30%] border-2 border-white rounded-sm text-white'>
+        <h1 className='text-xl text-center'>{props.user}</h1>
         {/* <h1 className='text-6xl text-center '>{props}</h1> */} 
         {/* JS can't render objects directly if we still want then we will have to convert it to json
         JSX sirf string, number, boolean, element render kar sakta hai — object nahi 

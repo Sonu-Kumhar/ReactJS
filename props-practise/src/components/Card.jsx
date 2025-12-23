16d79991c3a9adf95319d04257343bde9f5f3d0e
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-23 11:52:13 +0530

    React can't render the object directly

---
+++ props-practise/src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = (props) => {
+    console.log(props)
+  return (
+    <div className=' p-5 flex items-center justify-center bg-transparent border-2 border-white rounded-sm text-white'>
+        <h1 className='text-6xl text-center '>{props.user}</h1>
+        {/* <h1 className='text-6xl text-center '>{props}</h1> */} 
+        {/* JS can't render objects directly if we still want then we will have to convert it to json
+        JSX sirf string, number, boolean, element render kar sakta hai — object nahi 
+        <h1>{JSON.stringify(props)}</h1> */}
+    </div>
+  )
+}
+
+export default Card

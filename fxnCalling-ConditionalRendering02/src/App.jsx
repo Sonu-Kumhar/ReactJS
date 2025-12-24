8f0e18e7b46941f998cd8ab1dafb94622adc2cc6
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-24 12:32:33 +0530

    updated condition with 2 conditions

--- fxnCalling-ConditionalRendering02/src/App.jsx
+++ fxnCalling-ConditionalRendering02/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Pikachu from './components/Pikachu'
 import Raichu from './components/Raichu'
+import Pichu from './components/Pichu'
 
 const App = () => {
 
-  let user = "Ash";
+  let power = 10;
 
 
   return (
     <div>
-        {user=="Ash" ? <Pikachu/> : <Raichu/>}
+        {power >= 100 ? <Raichu/> : power >= 50 ? <Pikachu/> : <Pichu/>}
     </div>
   )
 }

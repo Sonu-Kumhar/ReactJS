6e432a027c9cbd8605f075756ff54b45b5d7f42f
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-27 13:01:58 +0530

    actual form handling with react

--- form-handling-04/src/App.jsx
+++ form-handling-04/src/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [user, setUser] = useState("")
 
   let submitHandler = (e) =>{
     e.preventDefault(); 
-    //browser ke default behaviour ko rokne ke liye e.preventDefault() use karte hai
-    //default behaviour jaise form submit hua to reload hona so usko rokne ke liye preventDefault() use krte hai
-    //e.preventDefault() browser ke default action ko rokta hai
-    console.log("form submitted");
+    console.log(user)
+    console.log("form submitted")
+  }
+
+  // Input controlled hai, state single hai, submit state ko read karta hai — code perfect hai
+
+  let writingSomething = (e) => {
+    setUser(e.target.value)
+    // console.log(e.target.value)
   }
 
   return (
     <div className='w-screen h-screen text-3xl p-10 bg-[#222] text-white'>
         <form onSubmit={submitHandler}>
-          <input className='border-2 border-white  px-4 py-2 rounded-xl'  type="text" placeholder='Enter your name'/>
+          <input
+           className='border-2 border-white  px-4 py-2 rounded-xl'  
+           type="text" 
+           placeholder='Enter your name'
+           onChange={writingSomething}
+           value={user}
+          />
           <button className='m-4 bg-emerald-400 px-4 py-2 rounded-xl active:scale-95 font-bold'>Submit</button>
         </form>
     </div>

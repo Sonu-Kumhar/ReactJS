729f2bb74c81cfb53eb6f53d5fd73a1ec2a69cf2
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-27 16:20:09 +0530

    made a name list and showing them on UI

--- form-handling-04/src/App.jsx
+++ form-handling-04/src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from 'react'
 
 const App = () => {
   const [user, setUser] = useState("")
+  const [allUsers, setAllUsers] = useState([])
 
   let submitHandler = (e) =>{
     e.preventDefault(); 
-    console.log(user)
-    console.log("form submitted")
+    console.log(user);
+    
+    setAllUsers([...allUsers, user]);
+    // console.log("form submitted")
+    // console.log("All users : ",allUsers);
+    setUser("")
   }
-
-  // Input controlled hai, state single hai, submit state ko read karta hai — code perfect hai
 
   let writingSomething = (e) => {
     setUser(e.target.value)
@@ -29,6 +32,12 @@ const App = () => {
           />
           <button className='m-4 bg-emerald-400 px-4 py-2 rounded-xl active:scale-95 font-bold'>Submit</button>
         </form>
+
+        {
+          allUsers.map((elem, index)=>{
+            return <h1 key={index}>{elem}</h1>
+          })
+        }
     </div>
   )
 }

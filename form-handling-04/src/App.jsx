97efaadf5fab64d8dd00c5efe307c65b0f9f98fb
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-28 16:59:20 +0530

    made a simple ToDo without using localstorage

--- form-handling-04/src/App.jsx
+++ form-handling-04/src/App.jsx
@@ -1,62 +1,54 @@
 import React from 'react'
 import { useState } from 'react'
-import Card from './components/Card'
 
 const App = () => {
-  const [pokemon, setPokemon] = useState("")
-  const [email, setEmail] = useState("")
-  const [allPokemon, setAllPokemon] = useState([])
+  const [task, setTask] = useState("")
+  const [allTask, setAllTask] = useState([])
 
   let submitHandler = (e) => {
-    e.preventDefault();
-    // console.log("form submitted")
-    setAllPokemon([...allPokemon, {pokemon, email}])
-    console.log(allPokemon);
-    // console.log(allPokemon)
-    setPokemon("")
-    setEmail("")
+      e.preventDefault();
+      // console.log("form submitted");
+      let newAllTask = [...allTask, task];
+      setAllTask(newAllTask);
+      // console.log(allTask)
+      setTask("")
   }
 
-  let writingPokemon = (e) => {
-    setPokemon(e.target.value)
-    // console.log(e.target.value)
-  }
-
-  let writingEmail = (e) => {
-    setEmail(e.target.value)
+  let deleteTask = (idx) => {
+    let newAllTask =  [...allTask];
+    newAllTask.splice(idx, 1);
+    console.log(newAllTask);
+    setAllTask(newAllTask);
+    console.log("task deleted clicked!!!", idx)
   }
 
   return (
-    <div className='w-screen h-screen bg-[#222] text-white p-10 flex flex-col gap-8'>
-      <form className='flex flex-col w-1/4 gap-5 text-2xl' onSubmit={submitHandler}>
-        <input 
-        type="text" 
-        placeholder='Enter Pokemon Name'
-        value={pokemon}
-        onChange={writingPokemon}
-        required
-        className='border-2 border-white px-4 py-2 rounded-xl'
-        />
-        <input 
-        type="text" 
-        placeholder='Enter Email'
-        value={email}
-        onChange={writingEmail}
-        className='border-2 border-white px-4 py-2 rounded-xl'
-        />
+    <div className='w-screen h-screen bg-[#252525] text-white p-10 flex flex-col items-center'>
+        <form className='w-full flex items-center justify-center mb-10' onSubmit={(e)=>{
+          submitHandler(e);
+        }}>
+          <input 
+          type="text"
+          placeholder='Enter task' 
+          className='text-white text-2xl px-4 py-2 border-2 border-white w-1/3 rounded-xl'
+          value={task}
+          required
+          onChange={(e)=>{
+            setTask(e.target.value)
+          }}
+          />
 
-        <button className='border-2 border-white bg-emerald-500 font-bold px-4 py-2 rounded-xl active:scale-95'>Add</button>
-      </form>
+          <button className='bg-emerald-500 px-4 py-2 text-2xl font-semibold rounded-xl mx-4 active:scale-95'>Add</button>
+        </form>
 
-      <div className='w-full h-80 flex flex-col gap-8 flex-wrap'>
         {
-        allPokemon.map((elem, index)=>{
-          return <div key={index}>
-            <Card name={elem.pokemon} email={elem.email}/>
-          </div>
-        })
-      }
-      </div>
+          allTask.map((elem, index)=>{
+            return <div key={index} className='flex w-1/3 gap-3 py-2 items-center justify-between'>
+              <h1 className='text-2xl font-bold'>{elem}</h1>
+              <button onClick={()=>{deleteTask(index)}} className='bg-red-500 px-2 py-1 rounded-lg font-bold active:scale-95'>Delete</button>
+            </div>
+          })
+        }
     </div>
   )
 }

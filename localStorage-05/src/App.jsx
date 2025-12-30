b6d93e634e4b288241827f1bc272f4fafcfa6e95
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-30 20:14:16 +0530

    added readme.md with a ui updation

--- localStorage-05/src/App.jsx
+++ localStorage-05/src/App.jsx
@@ -54,7 +54,7 @@ const App = () => {
         <input
           type="text"
           placeholder='Enter Quotes of the day'
-          className=' text-2xl rounded-sm px-4 py-2 w-1/2  border-1 border-black'
+          className=' text-2xl rounded-2xl px-4 py-2 w-1/2  border-1 border-black'
           value={quote}
           required
           onChange={(e) => {
@@ -62,18 +62,18 @@ const App = () => {
           }}
         />
 
-        <button className='bg-emerald-400/50 backdrop-blur-lg border-2 border-white/20 shadow-xl cursor-pointer  text-2xl rounded-lg px-4 py-2 text-[#202020] font-semibold active:scale-95'>{editIdx === null ? "Save" : "Update"}</button>
+        <button className='bg-blue-400/50 backdrop-blur-lg border-2 border-white/20 shadow-xl cursor-pointer  text-2xl rounded-2xl px-4 py-2 text-[#292929] font-semibold active:scale-95'>{editIdx === null ? "Save" : "Update"}</button>
       </form>
 
-      <div className="w-full font-semibold flex flex-wrap items-center justify-center gap-8">
+      <div className="w-1/2 font-semibold flex flex-wrap items-center justify-center gap-8">
         {
           allQuotes.map((elem, index) => {
             return <div key={index} className=" w-50 h-40 px-4 pt-4 pb-2 flex flex-col items-center justify-between  rounded-4xl bg-gradient-to-br from-white/30 to-white/20 backdrop-blur-2xl border border-white/30 shadow-[0_25px_50px_rgba(0,0,0,0.3)] ">
 
-              <h1 className="text-lg text-center text-[#2b2b2b] leading-6.5">{elem}</h1>
+              <h1 className="text-lg text-center text-[#363535] leading-6.5">{elem}</h1>
 
               <div className="w-full flex justify-end gap-2 mt-2 ">
-                <button onClick={() => { editQuote(index) }} className='bg-emerald-400/50 cursor-pointer text-[#2c2c2c] text-lg  rounded-lg px-2 py-1 font-bold active:scale-95'><img className="w-5" src="./editing.png" alt="edit" /></button>
+                <button onClick={() => { editQuote(index) }} className='bg-blue-400/50 cursor-pointer text-[#2c2c2c] text-lg  rounded-lg px-2 py-1 font-bold active:scale-95'><img className="w-5" src="./editing.png" alt="edit" /></button>
 
                 <button onClick={() => { deleteQuote(index) }} className='bg-red-500/50 cursor-pointer text-[#2c2c2c] text-lg rounded-lg px-2 py-1 font-bold active:scale-95'><img className="w-5" src="./trash.png" alt="" /></button>
               </div>

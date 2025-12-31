480dfe2e6fdfbbb5b242d043ac65848affd7c115
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-31 13:55:00 +0530

    setting up axios (js library) for HTTP request

---
+++ axios-06/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from "axios"
+
+const App = () => {
+  return (
+    <div className='text-2xl bg-emerald-500'>
+      app
+    </div>
+  )
+}
+
+// Axios is a JavaScript library used to make HTTP requests (GET, POST, PUT, DELETE) from:
+// Browser
+// Node.js
+
+// It is mainly used to connect frontend ↔ backend APIs.
+
+export default App

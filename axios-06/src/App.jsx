a5f92e751f6e3bb8340dee1f59438c304b5042d6
Sonu Kumhar <kumharsonu899@gmail.com>
2025-12-31 14:11:50 +0530

    getting the random title on every button click

--- axios-06/src/App.jsx
+++ axios-06/src/App.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import axios from "axios"
 
 const App = () => {
+
+  let getData = async () => {
+    let response = await axios("https://jsonplaceholder.typicode.com/todos/")
+    let randomNum = Math.floor(Math.random()*200)
+    console.log(response.data[randomNum].title)
+  }
+
   return (
-    <div className='text-2xl bg-emerald-500'>
-      app
+    <div className='w-screen h-screen text-white text-2xl bg-[#213448] p-10'>
+      <button onClick={getData} className='text-3xl border-2 bg-[#547792] px-4 py-2 rounded-xl active:scale-95'>Get Data</button>
     </div>
   )
 }

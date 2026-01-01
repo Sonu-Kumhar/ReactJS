4c0d9eb4fc730131e994388a4aaf3057d41209bd
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-01 18:30:45 +0530

    added an 3d effect on image and card on hover

--- useEffect-07/src/components/Card.jsx
+++ useEffect-07/src/components/Card.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 
-const Card = ({name, affiliation, gender, race, ki, image}) => {
-    let cardUrl = ["https://i.pinimg.com/736x/18/47/69/18476926ea4f05176521d48ef7dd95ad.jpg", 
+const Card = ({ name, affiliation, gender, race, ki, image }) => {
+    let cardUrl = ["https://i.pinimg.com/736x/18/47/69/18476926ea4f05176521d48ef7dd95ad.jpg",
         "https://i.pinimg.com/736x/d6/7e/7f/d67e7f219701eb3c3ec729cd28935fa2.jpg",
         "https://i.pinimg.com/736x/2a/8d/d4/2a8dd41fd6dd0717627c3da7e04ea3d6.jpg",
         "https://i.pinimg.com/736x/6b/7a/8a/6b7a8a3a93c230a169f09af244c6d25f.jpg"
     ]
     return (
         <div className='card w-70 h-105 rounded-lg'>
-            
-           <div className='relative w-full h-full  perspective-[1000px]'>
-            <img className='object-cover relative dragon-img' src="https://i.pinimg.com/736x/d6/7e/7f/d67e7f219701eb3c3ec729cd28935fa2.jpg" alt="" />
 
-            <div className='w-28'>
-                <img className='h-80 w-45 absolute top-20 left-28 object-contain object-center hover:scale-125 transition-all duration-[500ms]' src={image} alt="" />
+            <div className=' relative w-full h-full  [perspective:1000px]'>
+
+                <div className='card-frame w-full h-full transition-transform duration-500 [transform-style:preserve-3d]'>
+                    <img className='object-cover relative dragon-frame' src="https://i.pinimg.com/736x/d6/7e/7f/d67e7f219701eb3c3ec729cd28935fa2.jpg" alt="" />
+
+                    <div className='w-28'>
+                        <img className='h-80 w-45 absolute image top-20 left-28 object-contain object-center transition-all duration-[500ms]' src={image} alt="" />
+                    </div>
+                    <div className='absolute top-35 left-12 text-3xl px-2 font-semibold'>{name}</div>
+                    <div className='absolute top-46 left-12 text-2xl px-2 font-normal'>{affiliation}</div>
+                    <div className='absolute top-52 left-12 text-2xl px-2 font-normal'>{race}</div>
+                    <div className='absolute top-58 left-12 text-2xl px-2 font-normal'>{gender}</div>
+                    <div className='absolute top-64 left-12 text-2xl px-2 font-normal'>{ki}</div>
+                    <button className='absolute top-78 left-15 text-4xl px-2 text-white py-1 rounded-2xl active:scale-90 cursor-pointer'>Play</button>
+                </div>
+
             </div>
-            <div className='absolute top-35 left-12 text-3xl px-2 font-semibold'>{name}</div>
-            <div className='absolute top-46 left-12 text-2xl px-2 font-normal'>{affiliation}</div>
-            <div className='absolute top-52 left-12 text-2xl px-2 font-normal'>{race}</div>
-            <div className='absolute top-58 left-12 text-2xl px-2 font-normal'>{gender}</div>
-            <div className='absolute top-64 left-12 text-2xl px-2 font-normal'>{ki}</div>
-            <button className='absolute top-78 left-15 text-4xl px-2 text-white py-1 rounded-2xl active:scale-90 cursor-pointer'>Play</button>
-           </div>
+
         </div>
     )
 }

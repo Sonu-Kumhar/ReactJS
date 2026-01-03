195e21915137c6d0c471e5f94c9c5f6373d001fb
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-03 12:05:37 +0530

    setting up basic routing

---
+++ routing-08/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='text-9xl'>About Page</div>
+  )
+}
+
+export default About
---
+++ routing-08/src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-9xl'>Contact Page</div>
+  )
+}
+
+export default Contact

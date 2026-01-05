384efbcaf521fe5cf327ef5fbd03f87eb5fee875
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-05 12:01:02 +0530

    used NavLink instead of Link for active class

--- routing-08/src/Components/Navbar.jsx
+++ routing-08/src/Components/Navbar.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import orderIcon from "../assets/icons/order.png"
 
 const Navbar = () => {
   return (
     <div className='py-2 flex px-10 rounded-4xl justify-center text-3xl items-center'>
-        <div className='bg-emerald-500 w-fit rounded-4xl px-10 py-2 flex gap-10 items-center justify-center text-white font-semibold'>
-            <Link to="/">Home</Link>
-            <Link to="/products"> <img className='w-15' src={orderIcon} alt="Product.png" />Products</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
+      <div className='bg-[#3c3c3c] w-fit rounded-4xl px-10 py-2 flex gap-10 items-center justify-center text-white font-semibold'>
+        <NavLink to="/"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >Home
+        </NavLink>
+
+        <NavLink to="/products"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >Products
+        </NavLink>
+
+        <NavLink to="/about"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >About
+        </NavLink>
+
+        <NavLink to="/courses"
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >Courses
+        </NavLink>
+
+      </div>
     </div>
   )
 }

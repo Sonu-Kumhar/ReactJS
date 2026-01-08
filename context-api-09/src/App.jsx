102f3be5eb07c874987ef1c7a843da1ace5686d2
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-08 11:48:12 +0530

    fetched the data from a products api

--- context-api-09/src/App.jsx
+++ context-api-09/src/App.jsx
@@ -1,16 +1,30 @@
-import React, { useContext } from 'react'
-import Card from './Components/Card'
-import AllCourses from './Components/AllCourses'
-import { UserContextData } from './context/UserContext'
-
+import React from 'react'
+import axios from "axios"
+import { useState } from 'react'
 const App = () => {
-  const data = useContext(UserContextData)
+
+  const [allProducts, setAllProducts] = useState([])
+
+  let getAllProducts = async () => {
+    // let response = axios.get("https://fake-store-api.mock.beeceptor.com/api/products")
+    let response = await axios.get("https://api.escuelajs.co/api/v1/products")
+    let products = response.data;
+    setAllProducts([...products])
+    console.log(response.data);
+  }
+
   return (
-    <div className='text-3xl w-screen h-screen bg-red-200'>
-      <h1>App</h1>
-      <h2>{data}</h2>
-      <Card/>
-      <AllCourses/>
+    <div>
+      <button className='px-4 py-2 bg-emerald-500 ' onClick={getAllProducts}>Get All Products</button>
+      {
+        allProducts.map((elem, index)=>{
+          return <div className='flex gap-5'>
+            <img className='h-40' src={elem.images[0]} alt="" />
+            <h2>{elem.title}</h2>
+            <h3>{elem.price}</h3>
+          </div>
+        })
+      }
     </div>
   )
 }

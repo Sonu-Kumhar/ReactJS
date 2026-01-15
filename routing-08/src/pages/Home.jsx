3a696710589a1551f2b6371235f97c19e30fe0a6
Sonu Kumhar <kumharsonu899@gmail.com>
2026-01-15 23:56:02 +0530

    updated bg

--- routing-08/src/pages/Home.jsx
+++ routing-08/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-100% bg-[#0f172a] text-black text-6xl'>
+    <div className='w-screen h-100%  text-black text-6xl'>
         Home
     </div>
   )
